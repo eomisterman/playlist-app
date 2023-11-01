3af8117088415039e413879f978770e054e5a350
emilio ovalles-misterman <eomisterman@gmail.com>
2023-11-01 11:36:18 -0400

    removed tailwindcss in order to add chakraui

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
-
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
-};
-  
-export default config;
